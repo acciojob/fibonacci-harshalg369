a5080497fbcb16e2b6067d269cf562e185583f42
harshalg369 <hxrsh369@gmail.com>
2025-05-10 10:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 0;
-    if (num === 2) return 1;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
 
-    let a = 0, b = 1, next;
+  let a = 0, b = 1, next;
+  for (let i = 3; i <= num; i++) {
+    next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
+}
 
-    for (let i = 3; i <= num; i++) {
-        next = a + b;
-        a = b;
-        b = next;
-    }
+function findFibonacci() {
+  const input = document.getElementById("inputNumber").value;
+  const num = parseInt(input);
 
-    return b;
+  if (num >= 1 && num <= 50) {
+    const result = fibonacci(num);
+    document.getElementById("result").innerText = `Fibonacci(${num}) = ${result}`;
+  } else {
+    document.getElementById("result").innerText = "Please enter a number between 1 and 50.";
+  }
 }
