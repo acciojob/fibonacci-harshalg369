f6d10c07aa7e5285eb4e37c26459056e7f5f1f2c
harshalg369 <hxrsh369@gmail.com>
2025-05-12 13:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function fibonacci(num) {
-  if (num === 0) return 0;
-  if (num === 1) return 0;
-  if (num === 2) return 1;
+    if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 
-  let a = 0, b = 1;
-  for (let i = 3; i <= num; i++) {
-    let temp = a + b;
-    a = b;
-    b = temp;
-  }
-  return b;
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 }
 
-// Set results in HTML
-document.getElementById("result1").textContent = fibonacci(1);
-document.getElementById("result5").textContent = fibonacci(5);
-document.getElementById("result10").textContent = fibonacci(10);
+module.exports = fibonacci;
