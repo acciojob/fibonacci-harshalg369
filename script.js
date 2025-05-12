2892728208865cf2c10493ead1bd90f7cc1c8cb3
harshalg369 <hxrsh369@gmail.com>
2025-05-12 07:55:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function fibonacci(num) {
   if (num === 1) return 0;
   if (num === 2) return 1;
 
-  let a = 0, b = 1, temp;
+  let a = 0, b = 1;
   for (let i = 3; i <= num; i++) {
-    temp = a + b;
+    let temp = a + b;
     a = b;
     b = temp;
   }
